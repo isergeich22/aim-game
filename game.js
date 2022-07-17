2b889f902b705ab9b4d311623a597b15e2ad5130
isergeich22 <skiing1r@gmail.com>
2022-07-17 16:49:38 +0300

    code updates

--- game.js
+++ game.js
@@ -1,45 +1,64 @@
 // Game-Logic
+//{
+    const square = document.querySelector('.square')
+    const number = document.querySelector('.number')
+    const gOver = document.querySelector('.gOver')
+    let i = 0
 
-const square = document.querySelector('.square')
+    square.addEventListener('click', () => {
+        square.style.top = getRandomInt(0, 365) + 'px'
+        square.style.left = getRandomInt(0, 665) + 'px'
+        square.style.width = getRandomInt(8, 32) + 'px'
+        square.style.height = square.style.width
+        i++;
+        number.innerHTML = i;
+    })
 
-square.addEventListener('click', () => {
-    square.style.top = getRandomInt(0, 365) + 'px'
-    square.style.left = getRandomInt(0, 665) + 'px'
-})
+    function getRandomInt (min, max) {
+        min = Math.ceil(min)
+        max = Math.ceil(max)
 
-function getRandomInt (min, max) {
-    min = Math.ceil(min)
-    max = Math.ceil(max)
-
-    return Math.ceil(Math.random() * (max - min)) + min
-}
-
+        return Math.ceil(Math.random() * (max - min)) + min
+    }
+//}
 
 // Timer-Logic
-let timerInput = document.querySelector('#time')
-let buttonRun = document.querySelector('.start')
-let timerShow = document.querySelector('.time')
+// {
+    let timerInput = document.querySelector('#time')
+    let buttonRun = document.querySelector('.start')
+    let timerShow = document.querySelector('.time')
 
-buttonRun.addEventListener('click', () => {
-    timeMinut = parseInt(timerInput.value) * 60
-    timer = setInterval(() => {
-        seconds = timeMinut%60
-        minutes = timeMinut/60%60
-    
-        if (timeMinut <= 0) {
-            clearInterval(timer)
-    
-            square.style.display = 'none'
-    
-            alert('Время вышло!')
-            
-        } else {
-            let strTimer = `${Math.trunc(minutes)}:${seconds}`
-    
-            timerShow.innerHTML = strTimer
-        }
-        --timeMinut
-    }, 1000)
-    square.style.display = 'block'
-})
+    buttonRun.addEventListener('click', () => {        
+        square.style.top = getRandomInt(0, 365) + 'px'
+        square.style.left = getRandomInt(0, 665) + 'px'
+        number.innerHTML = 0
+        timeMinut = parseInt(timerInput.value) * 60
+        timer = setInterval(() => {
+            seconds = timeMinut%60
+            minutes = timeMinut/60%60
+        
+            if (timeMinut <= 0) {
 
+                clearInterval(timer)
+        
+                square.style.display = 'none'
+
+                gOver.style.display = 'flex'
+
+                timerShow.style.color = 'red'
+                timerShow.innerHTML = '0:0'
+
+                timerInput.innerHTML = ''
+                
+            } else {
+
+                let strTimer = `${Math.trunc(minutes)}:${seconds}`
+        
+                timerShow.innerHTML = strTimer
+
+            }
+            --timeMinut
+        }, 1000)
+        square.style.display = 'block'
+    })
+// }
